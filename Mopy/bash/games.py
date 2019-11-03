56686f53c9fd0891c7437f8841ce0b02614d7b8e
lojack5 <1458329+lojack5@users.noreply.github.com>
2019-11-03 10:28:19 -0700

    [py3] Reading from file already decoded.
    Note: Need to test malformed files to check it doesn't crash
    
    Traceback (most recent call last):
      File "..\Mopy\bash\bash.py", line 185, in main
        _main(opts)
      File "..\Mopy\bash\bash.py", line 361, in _main
        app.Init() # Link.Frame is set here !
      File "..\Mopy\bash\basher\__init__.py", line 4041, in Init
        self.InitData(progress)
      File "..\Mopy\bash\basher\__init__.py", line 4078, in InitData
        bosh.modInfos.refresh(booting=True)
      File "..\Mopy\bash\bosh\__init__.py", line 1915, in refresh
        lo_changed = self.refreshLoadOrder(
      File "..\Mopy\bash\bosh\__init__.py", line 1663, in _modinfos_cache_wrapper
        lord_func(self, *args, **kwargs)
      File "..\Mopy\bash\bosh\__init__.py", line 1770, in refreshLoadOrder
        load_order.refresh_lo(cached=not forceRefresh,
      File "..\Mopy\bash\load_order.py", line 305, in refresh_lo
        _update_cache(lo, active)
      File "..\Mopy\bash\load_order.py", line 254, in _update_cache
        lord, acti_sorted = _game_handle.get_load_order(lord, acti_sorted,
      File "..\Mopy\bash\games.py", line 227, in get_load_order
        lo, active = self._cached_or_fetch(cached_load_order,
      File "..\Mopy\bash\games.py", line 768, in _cached_or_fetch
        return self._fetch_load_order(cached_load_order, cached_active)
      File "..\Mopy\bash\games.py", line 778, in _fetch_load_order
        active, lo = self._parse_modfile(self.plugins_txt_path) # empty if not exists
      File "..\Mopy\bash\games.py", line 354, in _parse_modfile
        acti, _lo = _parse_plugins_txt_(path, self.mod_infos, _star=self._star)
      File "C:\Users\Lojack\Desktop\wrye-bash-dev\Mopy\bash\games.py", line 101, in _parse_plugins_txt_
        test2 = str(modname, encoding)
    TypeError: decoding str is not supported

--- Mopy/bash/games.py
+++ Mopy/bash/games.py
@@ -87,28 +87,10 @@ def _parse_plugins_txt_(path, mod_infos, _star):
             # use raw strings below
             is_active = not _star or modname.startswith('*')
             if _star and is_active: modname = modname[1:]
-            try:
-                test = bolt.decode(modname, encoding='cp1252')
-            except UnicodeError:
-                bolt.deprint('%r failed to properly decode' % modname)
-                continue
-            if bolt.GPath(test) not in mod_infos:
-                # The automatic encoding detector could have returned
-                # an encoding it actually wasn't.  Luckily, we
-                # have a way to double check: modInfos.data
-                for encoding in bolt.encodingOrder:
-                    try:
-                        test2 = str(modname, encoding)
-                        if bolt.GPath(test2) not in mod_infos:
-                            continue
-                        modname = bolt.GPath(test2)
-                        break
-                    except UnicodeError:
-                        pass
-                else:
-                    modname = bolt.GPath(test)
-            else:
-                modname = bolt.GPath(test)
+            # TODO(lojack): file is already open reading in as unicode (str)
+            # object.  Test to see if it chokes when weird characters are
+            # present.
+            modname = bolt.GPath(modname)
             modnames.append(modname)
             if is_active: active.append(modname)
     return active, modnames
