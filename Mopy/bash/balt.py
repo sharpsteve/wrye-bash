f596d0a7b6d8d7d35c3d606ec653bf9025649775
MrD <the.ubik@gmail.com>
2020-03-10 01:31:22 +0200

    Revert "NotebookPanel(WithFirstShow, PanelWin) XXXXXXX"
    This reverts commit ac86345faae544b387a76afff0b59cdee0c07b7b.

--- Mopy/bash/balt.py
+++ Mopy/bash/balt.py
@@ -47,7 +47,7 @@ from .gui import Button, CancelButton, CheckBox, HBoxedLayout, HLayout, \
     Label, LayoutOptions, OkButton, RIGHT, Stretch, TextArea, TOP, VLayout, \
     web_viewer_available, DialogWindow, WindowFrame, EventResult, ListBox, \
     Font, CheckListBox, UIListCtrl, PanelWin, Colors, DocumentViewer, \
-    ImageWrapper, BusyCursor, GlobalMenu, WrappingTextMixin, WithFirstShow
+    ImageWrapper, BusyCursor, GlobalMenu, WrappingTextMixin
 from .gui.base_components import _AComponent
 
 # Print a notice if wx.html2 is missing
@@ -2391,7 +2391,7 @@ class DnDStatusBar(wx.StatusBar):
         if event: event.Skip()
 
 #------------------------------------------------------------------------------
-class NotebookPanel(WithFirstShow, PanelWin):
+class NotebookPanel(PanelWin):
     """Parent class for notebook panels."""
     # UI settings keys prefix - used for sashPos and uiList gui settings
     keyPrefix = u'OVERRIDE'
