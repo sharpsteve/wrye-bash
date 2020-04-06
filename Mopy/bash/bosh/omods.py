b1346816609dd501bfb3fcc55308a1afa9dbbf8d
MrD <the.ubik@gmail.com>
2020-04-06 15:44:30 +0300

    open\(\' -> open\(u\' EEE some formatting
    These are internal should not affect users
    
    encoding\=\' -> encoding\=u\'
    
    TODO: get rid of encoding=u'utf-8-sig' - in (for instance):
    Mopy/bash/bosh/bain.py
    Mopy/bash/bosh/__init__.py
    Mopy/bash/basher/settings_links.py
    
    Try to trace uses
    
    Fixup for CsvReader TTT
    
    Traceback (most recent call last):
      File "bash\basher\patcher_dialog.py", line 171, in PatchExecute
        patchFile.init_patchers_data(enabled_patchers, SubProgress(progress, 0, 0.1)) #try to speed this up!
      File "bash\patcher\patch_files.py", line 139, in init_patchers_data
        patcher.initData(SubProgress(progress, index))
      File "bash\patcher\patchers\preservers.py", line 211, in initData
        self._parse_csv_sources(progress)
      File "bash\patcher\patchers\preservers.py", line 321, in _parse_csv_sources
        ImportActorsFactionsPatcher, self)._parse_csv_sources(progress)
      File "bash\patcher\patchers\preservers.py", line 103, in _parse_csv_sources
        parser_instance.readFromText(getPatchesPath(src_path))
      File "bash\parsers.py", line 398, in readFromText
        with CsvReader(textPath) as ins:
      File "bash\bolt.py", line 993, in __init__
        self.reader = csv.reader(CsvReader.utf_8_encoder(self.ins),excel_fmt,delimiter=delimiter)
    TypeError: "delimiter" must be string, not unicode
    
    So:
    
    @@ -987,11 +987,14 @@ def utf_8_encoder(unicode_csv_data):
         def __init__(self,path):
    -        self.ins = GPath(path).open(u'rb', encoding=u'utf-8-sig')
    -        excel_fmt = (u'excel', u'excel-tab')[b'\t' in self.ins.readline()]
    -        if excel_fmt == u'excel':
    -            delimiter = (u',', u';')[b';' in self.ins.readline()]
    -            self.ins.seek(0)
    -            self.reader = csv.reader(CsvReader.utf_8_encoder(self.ins),excel_fmt,delimiter=delimiter)
    +        self.ins = GPath(path).open(u'r', encoding=u'utf-8-sig')
    +        first_line = self.ins.readline()
    +        excel_fmt = b'excel-tab' if u'\t' in first_line else b'excel'
    +        self.ins.seek(0)
    +        if excel_fmt == b'excel':
    +            # TypeError: "delimiter" must be string, not unicode
    +            delimiter = b';' if b';' in first_line else b','
    +            self.reader = csv.reader(CsvReader.utf_8_encoder(self.ins),
    +                                     excel_fmt, delimiter=delimiter)
             else:
    -            self.ins.seek(0)
    -            self.reader = csv.reader(CsvReader.utf_8_encoder(self.ins),excel_fmt)
    +            self.reader = csv.reader(CsvReader.utf_8_encoder(self.ins),
    +                                     excel_fmt)
    
    @@ -1001,4 +1004,4 @@ def __exit__(self, exc_type, exc_value, exc_traceback): self.ins.close()
         def __iter__(self):
    -        for iter in self.reader:
    -            yield [unicode(x,'utf8') for x in iter]
    +        for row in self.reader:
    +            yield [unicode(x, u'utf8') for x in row]

--- Mopy/bash/bosh/omods.py
+++ Mopy/bash/bosh/omods.py
@@ -170,13 +170,13 @@ class OmodFile(object):
             self.writeInfo(ocdDir.join(u'info.txt'), self.omod_path.stail, extractDir.join(u'readme').exists(), extractDir.join(u'script').exists())
             progress(0.47, self.omod_path.stail + u'\n' + _(u'Creating omod conversion data') + u'\nscript')
             if extractDir.join(u'script').exists():
-                with open(extractDir.join(u'script').s, 'rb') as ins:
-                    with ocdDir.join(u'script.txt').open('w') as output:
+                with open(extractDir.join(u'script').s, u'rb') as ins:
+                    with ocdDir.join(u'script.txt').open(u'w') as output:
                         output.write(_readNetString(ins))
             progress(0.48, self.omod_path.stail + u'\n' + _(u'Creating omod conversion data') + u'\nreadme.rtf')
             if extractDir.join(u'readme').exists():
-                with open(extractDir.join(u'readme').s, 'rb') as ins:
-                    with ocdDir.join(u'readme.rtf').open('w') as output:
+                with open(extractDir.join(u'readme').s, u'rb') as ins:
+                    with ocdDir.join(u'readme.rtf').open(u'w') as output:
                         output.write(_readNetString(ins))
             progress(0.49, self.omod_path.stail + u'\n' + _(u'Creating omod conversion data') + u'\nscreenshot')
             if extractDir.join(u'image').exists():
@@ -252,7 +252,7 @@ class OmodFile(object):
         # Extract data stream to an uncompressed stream
         subprogress = bolt.SubProgress(progress,0,0.3,full=dataPath.size)
         subprogress(0, self.omod_path.stail + u'\n' + _(u'Unpacking %s') % dataPath.stail)
-        with dataPath.open('rb') as ins:
+        with dataPath.open(u'rb') as ins:
             done = 0
             with open(outPath.join(dataPath.sbody+u'.tmp').s, u'wb') as output:
                 # Decoder properties
