83176e651f6d3682387cfc0292b93abc371a6a33
MrD <the.ubik@gmail.com>
2020-05-13 21:57:59 +0300

    TTT decoder return encoding in unicode XXX

--- Mopy/bash/localize.py
+++ Mopy/bash/localize.py
@@ -313,7 +313,7 @@ def format_date(secs): # type: (float) -> unicode
     except ValueError: # local time in windows can't handle negative values
         local = time.gmtime(secs)
     return bolt.decoder(time.strftime(u'%c', local), ##: decoder?
-                        locale.getpreferredencoding(do_setlocale=False))
+        unicode(locale.getpreferredencoding(do_setlocale=False)))
 
 # PY3: Probably drop in py3?
 def unformat_date(date_str):
