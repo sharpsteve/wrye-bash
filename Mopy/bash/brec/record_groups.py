6a8a101f884ba84c38d41773eaaf21c803d0e5bb
MrD <the.ubik@gmail.com>
2020-05-16 19:12:17 +0300

    Revert "Only allow MobBase.header being a GrupHeader XXX TTT"
    This reverts commit ac958de62cd33d5c8602a8b575ebbf6e74ca4f9d.
    
    mod_files.py  241 load: Error in Unofficial Oblivion Patch.esp
    Traceback (most recent call last):
      File "bash\mod_files.py", line 234, in load
        self.tops[label].load(ins, do_unpack and (topClass != MobBase))
      File "bash\record_groups.py", line 86, in load
        ins.tell() + self.size - RecordHeader.rec_header_size)
      File "bash\record_groups.py", line 1324, in load_rec_group
        worldBlock = MobWorld(header,selfLoadFactory,world,ins,True)
      File "bash\record_groups.py", line 1003, in __init__
        MobCells.__init__(self, header, loadFactory, ins, do_unpack)
      File "bash\record_groups.py", line 749, in __init__
        MobBase.__init__(self, header, loadFactory, ins, do_unpack)
      File "bash\record_groups.py", line 74, in __init__
        if ins: self.load(ins, do_unpack)
      File "bash\record_groups.py", line 86, in load
        ins.tell() + self.size - RecordHeader.rec_header_size)
      File "bash\record_groups.py", line 1055, in load_rec_group
        cellBlock = MobCell(header, selfLoadFactory, cell)
      File "bash\record_groups.py", line 465, in __init__
        MobBase.__init__(self, header, loadFactory, ins, do_unpack)
      File "bash\record_groups.py", line 67, in __init__
        u'got:%r' % (self, header))
    ArgumentError: You need a GRP header to instantiate <bash.record_groups.MobCell object at 0x0000000015AC87D8> - got:<Record Header: [CELL:00005FC6] v0>

--- Mopy/bash/brec/record_groups.py
+++ Mopy/bash/brec/record_groups.py
@@ -47,12 +47,13 @@ class MobBase(object):
     def __init__(self, header, loadFactory, ins=None, do_unpack=False):
         self.header = header
         self.size = header.size
-        try:
+        if header.recType == b'GRUP':
             self.label, self.groupType, self.stamp = (
                 header.label, header.groupType, header.stamp)
-        except AttributeError:
-            raise ArgumentError(u'You need a GRP header to instantiate %r - '
-                                u'got:%r' % (self, header))
+        else: # TODO(ut) should MobBase used for *non* GRUP headers??
+            # Yes it's weird, but this is how it needs to work
+            self.label, self.groupType, self.stamp = (
+                header.flags1, header.fid, header.flags2)
         self.debug = False
         # binary blob of the whole record group minus its GRUP header ##: rename
         self.data = None
