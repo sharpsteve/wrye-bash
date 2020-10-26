ea5c1b9fa46c5bee84483360a997ea79114ae412
MrD <the.ubik@gmail.com>
2020-10-26 14:03:43 +0200

    Mopy/bash/record_groups.py XXX TTT?
    hack for info label - catch_errors? EEE FFF

--- Mopy/bash/mod_files.py
+++ Mopy/bash/mod_files.py
@@ -189,7 +189,7 @@ class ModFile(object):
         self.longFids = False
 
     def load(self, do_unpack=False, progress=None, loadStrings=True,
-             catch_errors=True):
+             catch_errors=False): # TODO: let it blow?
         """Load file."""
         from . import bosh
         progress = progress or bolt.Progress()
