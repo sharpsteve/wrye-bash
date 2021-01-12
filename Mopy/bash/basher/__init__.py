2373088f205b008aa0ea25762bbccae63db8f83f
MrD <the.ubik@gmail.com>
2021-01-12 15:30:06 +0200

    WIP ListInfo validate_filename_str EEE
    I had to add _valid_exts_re to ModInfo - let's see -- BSA ext? EEE
    
    What a joy - note:
    Mopy/bash/basher/__init__.py:
    - self.fileStr[-4:] should always match the displayed mod/save info
    name.ext (we are renaming in the details panel).
    - ugly __ext_group moved to model where it belongs and became beautiful
    - validate_filename became semi-static validate_filename_str except when
    used via _validate_filename where it became validate_name - contains
    instance validation
    - renaming_type was already been used - this and __ext_group naturally
    fit a class method - so old validate_filename params simplified.
    
    Mopy/bash/bosh/__init__.py:
    Should be added to bsa_info - mods we don't rename en masse
    Mopy/bash/bosh/bain.py:
    Yep Installers calling AFile - from the Gui backdoor

--- Mopy/bash/basher/__init__.py
+++ Mopy/bash/basher/__init__.py
@@ -1342,20 +1342,14 @@ class _EditableMixinOnFileInfos(_EditableMixin):
         fileStr = self._fname_ctrl.text_content
         if fileStr == self.fileStr: return
         #--Validate the filename
-        if not self._validate_filename(fileStr, self.fileStr[-4:].lower()):
-            self._fname_ctrl.text_content = self.fileStr
-        #--Else file exists?
-        elif self.file_info.dir.join(fileStr).exists():
-            balt.showError(self,_(u"File %s already exists.") % fileStr)
+        root, newName, _numStr = self.file_info.validate_name(fileStr)
+        if not root:
+            balt.showError(self, newName)
             self._fname_ctrl.text_content = self.fileStr
         #--Okay?
         else:
             self.fileStr = fileStr
             self.SetEdited()
-
-    def _validate_filename(self, fileStr, single_ext):
-        return self.panel_uilist.validate_filename(
-            fileStr, single_ext=single_ext)[0]
 
     def OnFileEdit(self, new_text):
         """Event: Editing filename."""
@@ -2004,22 +1998,14 @@ class SaveList(balt.UIList):
                                                      u'pcLocation')),
     ])
 
-    __ext_group = u'(\.(' + bush.game.Ess.ext[1:] + u'|' + \
-                  bush.game.Ess.ext[1:-1] + u'r' + u'))' # add bak !!!
-    def validate_filename(self, name_new, has_digits=False, ext=u'',
-                          is_filename=True, _old_path=None, single_ext=u''):
-        if _old_path and bosh.bak_file_pattern.match(_old_path.s): #TODO: renaming bak
-            balt.showError(self, _(u'Renaming bak files is not supported.'))
-            return None, None, None ##: this used to not-Skip now it Vetoes
-        return super(SaveList, self).validate_filename(name_new,
-            has_digits=has_digits, ext=self.__ext_group,
-            is_filename=is_filename, single_ext=single_ext)
-
     def OnLabelEdited(self, is_edit_cancelled, evt_label, evt_index, evt_item):
         """Savegame renamed."""
         if is_edit_cancelled: return EventResult.FINISH # todo CANCEL?
-        root, newName, _numStr = self.validate_filename(evt_label)
-        if not root: return EventResult.CANCEL # validate_filename would Veto
+        root, newName, _numStr = \
+            self.panel.detailsPanel.file_info.validate_filename_str(evt_label)
+        if not root:
+            balt.showError(self, newName)
+            return EventResult.CANCEL # validate_filename would Veto
         item_edited = [self.panel.detailsPanel.displayed_item]
         selected = [s for s in self.GetSelected() if
                     not bosh.bak_file_pattern.match(s.s)] # YAK !
@@ -2197,10 +2183,6 @@ class SaveDetails(_ModsSavesDetails):
         """Info field was edited."""
         if self.saveInfo and self.gInfo.modified:
             self.saveInfo.set_table_prop(u'info', new_text)
-
-    def _validate_filename(self, fileStr, single_ext):
-        return self.panel_uilist.validate_filename(fileStr,
-            _old_path=self.saveInfo.name, single_ext=single_ext)[0]
 
     def testChanges(self): # used by the master list when editing is disabled
         saveInfo = self.saveInfo
@@ -2392,19 +2374,16 @@ class InstallersList(balt.UIList):
             uilist_ctrl.ec_set_selection(*selection_span)
             return EventResult.FINISH  ##: needed?
 
-    __ext_group = \
-        r'(\.(' + u'|'.join(ext[1:] for ext in archives.readExts) + u')+)'
     def OnLabelEdited(self, is_edit_cancelled, evt_label, evt_index, evt_item):
         """Renamed some installers"""
         if is_edit_cancelled: return EventResult.FINISH ##: previous behavior todo TTT
         selected = self.GetSelected()
+        # common to all selected! enforced in OnBeginEditLabel
         renaming_type = type(self.data_store[selected[0]])
-        installables = self.data_store.filterInstallables(selected)
-        kwargs = {u'is_filename': bool(installables)}
-        if renaming_type.is_archive():
-            kwargs[u'ext'] = self.__ext_group
-        root, newName, _numStr = self.validate_filename(evt_label, **kwargs)
-        if not root: return EventResult.CANCEL
+        root, newName, _numStr = renaming_type.validate_filename_str(evt_label)
+        if not root:
+            balt.showError(self, newName)
+            return EventResult.CANCEL
         #--Rename each installer, keeping the old extension (for archives)
         with BusyCursor():
             refreshes, ex = [(False, False, False)], None
@@ -3303,8 +3282,6 @@ class ScreensList(balt.UIList):
     global_links = OrderedDefaultDict(lambda: Links()) # Global menu
     _shellUI = True
     _editLabels = True
-    __ext_group = \
-        r'(\.(' + u'|'.join(ext[1:] for ext in bosh.imageExts) + u')+)'
 
     _sort_keys = {u'File'    : None,
                   u'Modified': lambda self, a: self.data_store[a].mtime,
@@ -3328,9 +3305,12 @@ class ScreensList(balt.UIList):
     def OnLabelEdited(self, is_edit_cancelled, evt_label, evt_index, evt_item):
         """Rename selected screenshots."""
         if is_edit_cancelled: return EventResult.CANCEL
-        root, _newName, numStr = self.validate_filename(evt_label, has_digits=True,
-                                                        ext=self.__ext_group)
-        if not (root or numStr): return # allow for number only names
+        root, newName, numStr = \
+            self.panel.detailsPanel.file_info.validate_filename_str(
+                evt_label, has_digits=True)
+        if not (root or numStr):
+            balt.showError(self, newName)
+            return # allow for number only names
         selected = self.GetSelected()
         #--Rename each screenshot, keeping the old extension
         num = int(numStr or  0)
--- Mopy/bash/bosh/__init__.py
+++ Mopy/bash/bosh/__init__.py
@@ -99,6 +99,49 @@ imageExts = {u'.gif', u'.jpg', u'.png', u'.jpeg', u'.bmp', u'.tif'}
 #------------------------------------------------------------------------------
 # File System -----------------------------------------------------------------
 #------------------------------------------------------------------------------
+class ListInfo(object):
+    """Info object displayed in Wrye Bash list."""
+    __slots__ = ()
+    _valid_exts_re = u''
+    _is_filename = True
+
+    @classmethod
+    def validate_filename_str(cls, name_str, has_digits=False):
+        """Basic validation of list item name - those are usually filenames so
+        they should contain valid chars. We also optionally check for match
+        with an extension group (apart from projects and markers).
+
+        :type name_str: unicode
+        """
+        if not name_str:
+            return None, _(u'Empty name !'), None
+        char = cls._is_filename and bolt.Path.has_invalid_chars(name_str)
+        if char:
+            return None, _(u'%(new_name)s contains invalid character '
+                u'(%(char)s)') % {u'new_name': name_str, u'char': char}, None
+        # require at least one char before extension
+        regex = u'^%s(.*?)' % (u'' r'(?=.+\.)' if cls._valid_exts_re else u'')
+        if has_digits: regex += u'' r'(\d*)'
+        regex += cls._valid_exts_re + u'$'
+        rePattern = re.compile(regex, re.I | re.U)
+        maPattern = rePattern.match(name_str)
+        if maPattern:
+            num_str = maPattern.groups()[1] if has_digits else None
+        if not maPattern or not (maPattern.groups()[0] or num_str):
+            return None, (_(u'Bad extension or file root: ') + name_str), None
+        return maPattern.groups()[0], GPath(name_str), num_str
+
+    def validate_name(self, name_str):
+        # disallow extension change but not if no-extension info type
+        check_ext = name_str and self.__class__._valid_exts_re
+        if check_ext and not name_str.lower().endswith(self.abs_path.cext):
+            return None, _(u'%s: Incorrect file extension (must be %s)') % (
+                name_str, self.abs_path.ext), None
+        #--Else file exists?
+        if self.dir.join(name_str).exists(): ##: check using file_infos
+            return None, _(u'File %s already exists.') % name_str, None
+        return self.__class__.validate_filename_str(name_str)
+
 class MasterInfo(object):
     """Slight abstraction over ModInfo that allows us to represent masters that
     are missing an active mod counterpart."""
@@ -151,7 +194,7 @@ class MasterInfo(object):
         return u'%s<%r>' % (self.__class__.__name__, self.curr_name)
 
 #------------------------------------------------------------------------------
-class FileInfo(AFile):
+class FileInfo(AFile, ListInfo):
     """Abstract Mod, Save or BSA File. Features a half baked Backup API."""
     _null_stat = (-1, None, None)
 
@@ -363,6 +406,8 @@ reBashTags = re.compile(u'{{ *BASH *:[^}]*}}\\s*\\n?',re.U)
 class ModInfo(FileInfo):
     """A plugin file. Currently, these are .esp, .esm, .esl and .esu files."""
     _has_esm_flag = _is_esl = False # Cached, since we need it so often
+    _valid_exts_re = r'(\.(' + u'|'.join(
+        x[1:] for x in bush.game.espm_extensions) + u')+)'
 
     def __init__(self, fullpath, load_cache=False):
         self.isGhost = endsInGhost = (fullpath.cs[-6:] == u'.ghost')
@@ -1105,6 +1150,8 @@ from . import cosaves
 class SaveInfo(FileInfo):
     cosave_types = () # cosave types for this game - set once in SaveInfos
     _cosave_ui_string = {PluggyCosave: u'XP', xSECosave: u'XO'} # ui strings
+    _valid_exts_re = u'(\.(' + bush.game.Ess.ext[1:] + u'|' + \
+                     bush.game.Ess.ext[1:-1] + u'r' + u'))' # add bak !!!
 
     def __init__(self, fullpath, load_cache=False):
         # Dict of cosaves that may come with this save file. Need to get this
@@ -1112,6 +1159,12 @@ class SaveInfo(FileInfo):
         # for SSE and FO4
         self._co_saves = self.get_cosaves_for_path(fullpath)
         super(SaveInfo, self).__init__(fullpath, load_cache)
+
+    def validate_name(self, name_str):
+        # TODO: renaming bak would drop this override
+        if bak_file_pattern.match(self.name.s):
+            return None, _(u'Renaming bak files is not supported.'), None
+        return super(SaveInfo, self).validate_name(name_str)
 
     def getFileInfos(self): return saveInfos
 
@@ -1271,6 +1324,8 @@ class SaveInfo(FileInfo):
 class ScreenInfo(FileInfo):
     """Cached screenshot, stores a bitmap and refreshes it when its cache is
     invalidated."""
+    _valid_exts_re = r'(\.(' + u'|'.join(ext[1:] for ext in imageExts) + u')+)'
+
     def __init__(self, fullpath, load_cache=False):
         self.cached_bitmap = None
         super(ScreenInfo, self).__init__(fullpath, load_cache)
