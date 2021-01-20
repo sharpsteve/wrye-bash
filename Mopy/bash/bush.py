6c8a614b07b3c3003acd94f85eb9784e13e796d8
Infernio <infernio@icloud.com>
2021-01-20 21:10:32 +0100

    Fix __import__ usage
    Needs 1 passed instead of -1 on py3. The game select dialog shows now!

--- Mopy/bash/bush.py
+++ Mopy/bash/bush.py
@@ -61,7 +61,7 @@ def _supportedGames():
         if not ispkg: continue # game support modules are packages
         # Equivalent of "from game import <modname>"
         try:
-            module = __import__(u'game',globals(),locals(),[modname],-1)
+            module = __import__(u'game', globals(), locals(), [modname], 1)
             submod = getattr(module,modname)
             game_type = submod.GAME_TYPE
             _allModules[game_type.displayName] = submod
