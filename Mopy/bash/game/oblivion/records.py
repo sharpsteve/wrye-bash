624bff67d8e1413791a12867d0ae25020a163b67
MrD <the.ubik@gmail.com>
2021-02-06 02:23:16 +0200

    (_flags,'flags',0)

--- Mopy/bash/game/oblivion/records.py
+++ Mopy/bash/game/oblivion/records.py
@@ -524,7 +524,7 @@ class MreAlch(MelRecord,MreHasEffects):
         MelIcon(),
         MelScript(),
         MelFloat(b'DATA', 'weight'),
-        MelStruct(b'ENIT','iB3s','value',(_flags,'flags',0),('unused1',null3)),
+        MelStruct(b'ENIT','iB3s','value',(_flags, u'flags'),('unused1',null3)),
         MelEffects(),
         MelEffectsObmeFull(),
     ).with_distributor(_effects_distributor)
@@ -765,7 +765,7 @@ class MreCont(MreWithItems):
         MelModel(),
         MelScript(),
         MelItems(),
-        MelStruct(b'DATA','=Bf',(_flags,'flags',0),'weight'),
+        MelStruct(b'DATA','=Bf',(_flags, u'flags'),'weight'),
         MelFid(b'SNAM','soundOpen'),
         MelFid(b'QNAM','soundClose'),
     )
@@ -818,7 +818,7 @@ class MreCrea(MreActorBase):
         MelStrings(b'NIFZ','bodyParts'),
         MelBase(b'NIFT','nift_p'), # Texture File Hashes
         MelStruct(b'ACBS','=I3Hh2H',
-            (_flags,'flags',0),'baseSpell','fatigue','barterGold',
+            (_flags, u'flags'),'baseSpell','fatigue','barterGold',
             ('level',1),'calcMin','calcMax'),
         MelGroups(u'factions',
             MelStruct(b'SNAM', u'IB3s', (FID, u'faction'), u'rank',
@@ -1152,7 +1152,7 @@ class MreIngr(MelRecord,MreHasEffects):
         MelIcon(),
         MelScript(),
         MelFloat(b'DATA', 'weight'),
-        MelStruct(b'ENIT','iB3s','value',(_flags,'flags',0),('unused1',null3)),
+        MelStruct(b'ENIT','iB3s','value',(_flags, u'flags'),('unused1',null3)),
         MelEffects(),
         MelEffectsObmeFull(),
     ).with_distributor(_effects_distributor)
@@ -1385,7 +1385,7 @@ class MreNpc(MreActorBase):
         MelFull(),
         MelModel(),
         MelStruct(b'ACBS','=I3Hh2H',
-            (_flags,'flags',0),'baseSpell','fatigue','barterGold',
+            (_flags, u'flags'),'baseSpell','fatigue','barterGold',
             ('level',1),'calcMin','calcMax'),
         MelGroups(u'factions',
             MelStruct(b'SNAM', u'IB3s', (FID, u'faction'), u'rank',
@@ -1974,7 +1974,7 @@ class MreWeap(MelRecord):
         MelScript(),
         MelFid(b'ENAM','enchantment'),
         MelOptUInt16(b'ANAM', 'enchantPoints'),
-        MelStruct(b'DATA','I2f3IfH','weaponType','speed','reach',(_flags,'flags',0),
+        MelStruct(b'DATA','I2f3IfH','weaponType','speed','reach',(_flags, u'flags'),
             'value','health','weight','damage'),
     )
     __slots__ = melSet.getSlotsUsed()
